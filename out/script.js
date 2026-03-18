f39890519387cdaee8e0908cf4f2cde958be1f50
Marina Au <marinatnau@gmail.com>
2026-03-17 23:24:06 -0400

    Implemented designs (with placeholders) for the Our Team page

--- out/script.js
+++ out/script.js
@@ -1,5 +1,6 @@
+var navText = document.getElementById("nav-menu-text");
+
 toggleMenu = () => {
-    var navText = document.getElementById("nav-menu-text");
     console.log(navText);
     if(navText.textContent === "MENU") {
         navText.textContent = "CLOSE";
